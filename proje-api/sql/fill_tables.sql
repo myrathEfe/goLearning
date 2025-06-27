410b72e35c2a3751c79ed539f2c188f55c3928fd
Murat Efe <meyalniz@icloud.com>
2025-06-27 19:24:48 +0300

    project

---
+++ proje-api/sql/fill_tables.sql
@@ -0,0 +1,10 @@
+INSERT INTO roles(role_name) VALUES('teacher');
+INSERT INTO roles(role_name) VALUES('student');
+
+
+INSERT INTO users(name,surname,username,email,role_id) VALUES
+('John','Doe','jdoe','johndoe@gmail.com',1),
+('Elise','Doe','elise','elise@gmail.com',2),
+('Kate','Tri','ktri','kate@gmail.com',2),
+('Mark','Philip','mark_p','mark_p@yahoo.com',2),
+('Arif','Kemal','arif38','arif38@hotmail.com',2);
